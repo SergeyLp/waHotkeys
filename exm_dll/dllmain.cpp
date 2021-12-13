1a575cfe0753d7f1528cd52fe74299f3a0bdbc95
Martin <lipsergey@gmail.com>
2021-12-13 15:09:30 +0300

    Initial

---
+++ exm_dll/dllmain.cpp
@@ -0,0 +1,34 @@
+#include "pch.h"
+
+struct GenPlugins {
+    int version;                    //Out
+    char* description;          //Out
+    int(*init)();                   //Out
+    void(*config)();            //Out
+    void(*quit)();               //Out
+    HWND hwndParent;        //In
+    HINSTANCE hDllInstance;//In
+};
+
+static int init() {
+    MessageBeep(0xFFFFFFFF);
+    return 0;
+}
+static void config(){}
+static void quit(){}
+
+static GenPlugins plugin = {
+    0x10, nullptr,
+    init, config, quit,
+    0, 0
+};
+
+extern "C" {
+    __declspec(dllexport) GenPlugins* winampGetGeneralPurposePlugin() {
+
+        static char desc[] = "·• Ly Ascetic Hotkeys •·";
+        plugin.description = desc;
+
+        return &plugin;
+    }
+}
---
+++ stub_exe/pch.h
@@ -0,0 +1,6 @@
+#pragma once
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from 
+#include <tchar.h>
+#include <Windows.h>
+#include <stdio.h>
+
